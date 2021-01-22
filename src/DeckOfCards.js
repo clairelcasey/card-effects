00ff7e3b216d90d5d9b8b7e1e9fbb3c6aefad2c2
Tracy Jiang <tracyjiangmx@gmail.com>
2021-01-22 14:42:34 -0800

    completed part 1

--- src/DeckOfCards.js
+++ src/DeckOfCards.js
@@ -22,10 +22,15 @@ function DeckOfCards({ deckId }) {
 
   const [cardsDrawn, setCardsDrawn] = useState([]);
   const [shouldDraw, setShouldDraw] = useState(false);
+  const [shouldShuffle, setShouldShuffle] = useState(false);
   console.log('DeckOfCards top, state cardsDrawn:', cardsDrawn);
 
-  function handleClick() {
+  function handleDraw() {
     setShouldDraw(true);
+  }
+
+  function handleShuffle() {
+    setShouldShuffle(true);
   }
 
   useEffect(function drawNewCard() {
@@ -43,11 +48,24 @@ function DeckOfCards({ deckId }) {
 
   }, [shouldDraw, deckId]);
 
+
+  useEffect(function shuffleDeck() {
+    async function shuffle() {
+      const cardResult = await axios.get(`${BASE_URL}${deckId}/shuffle`);
+      console.log('effect draw(), cardResult:', cardResult);
+
+
+    }
+
+    if(shouldShuffle) shuffle();
+  }, [shouldShuffle]);
+
+
   const cards = cardsDrawn.map( card => <Card key={card.code} card={card} />);
 
   return (
     <div className="DeckOfCards" >
-      {(cardsDrawn.length < 52) && <button onClick={handleClick}>Add a card!</button>}
+      {(cardsDrawn.length < 52) && <button onClick={handleDraw}>Add a card!</button>}
       {cards}
     </div>
   )
